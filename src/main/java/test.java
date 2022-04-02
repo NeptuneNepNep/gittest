854e6f3c81d205baace52c05642240dab48842d1
tom_glb <clearlove2300@163.com>
2022-04-02 15:46:21 +0800

    my third commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("welcome to git");
         System.out.println("my second version");
+        System.out.println("third");
     }
 }
